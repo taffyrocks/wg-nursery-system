d8c524c55c94c575cf41fd2bf5f0de0ea6c8289a
taffyrocks <tafadzwa.chitava@worngundidj.org.au>
2025-02-22 16:05:22 +1100

    Update Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import { AuthProvider } from '@/components/AuthProvider'
+import { AuthProvider } from '../components/AuthProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
