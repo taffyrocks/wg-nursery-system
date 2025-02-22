2733bfb3349950f064314c7095b915f1005beb73
taffyrocks <tafadzwa.chitava@worngundidj.org.au>
2025-02-22 19:46:48 +1100

    Update Next Config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': require('path').resolve(__dirname, 'src'), // Resolves '@' to the 'src' directory
-    };
-    return config;
-  },
 }
 
-module.exports = nextConfig;
+module.exports = nextConfig
