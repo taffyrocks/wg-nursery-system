c1e0bd2dc186944a232dd9dec64fbb473c98897f
taffyrocks <tafadzwa.chitava@worngundidj.org.au>
2025-02-22 21:03:54 +1100

    Update Login Page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '@/lib/firebase';
+import { auth } from '../../lib/firebase';
 import { useRouter } from 'next/navigation';
 
 export default function Login() {
