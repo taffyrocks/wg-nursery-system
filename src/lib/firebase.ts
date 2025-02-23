bb9335c795496e46a01d8d8baf77014296efef41
taffyrocks <tafadzwa.chitava@worngundidj.org.au>
2025-02-23 15:16:09 +1100

    Update Firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -10,7 +10,7 @@ const firebaseConfig = {
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "wg-nursery-system",
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "wg-nursery-system.firebasestorage.app",
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "475342845298",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:475342845298:web:7e640e00d5656a544a59eb"
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:475342845298:web:7e640e00d5656a544a59eb",
   measurementId: "G-5FCTGV9XR6"
 };
 
